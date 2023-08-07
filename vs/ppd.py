de7fff27072c48e7449da9da487e83ca51f578e4
Pratyush Goutam <113873593+prat555@users.noreply.github.com>
2023-08-07 13:04:42 +0530

    Delete vs directory

--- vs/ppd.py
+++
@@ -1,8 +0,0 @@
-print("hello")
-print("write")
-import tkinter as tk
-r = tk.Tk()
-r.title('Counting Seconds')
-button = tk.Button(r, text='Stop', width=25, command=r.destroy)
-button.pack()
-r.mainloop()
